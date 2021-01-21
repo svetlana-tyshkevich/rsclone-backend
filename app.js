a1c36ed03c7b6967a6dcedec0cefbb776d52d80b
Svetlana Tyshkevich <panfilova.ss@gmail.com>
2021-01-21 18:02:28 +0200

    fix: fix port

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.use(express.json({ extended: true}));
 
 app.use('/api/auth', require('./routes/auth.routes'));
 
-const PORT = config.get('port') || 5000;
+const PORT = process.env.PORT || 5000;
 
 
 async function start() {
